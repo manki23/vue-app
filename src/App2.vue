c80dcb6888acde45343f02591eac0a5cbe6a4758
Myriam ANKI <manki@e2r7p22.42.fr>
2020-10-05 14:47:53 +0200

    First draft of the decision helper

---
+++ src/App2.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <myFirstComponent />
+</div>
+</template>
+
+<script>
+import myFirstComponent from "./components/myFirstComponent.vue";
+
+export default {
+    name: "App",
+    components: {
+        myFirstComponent,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/myFirstComponent.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="hello">
+    <h1>{{ title }}</h1>
+    <button v-on:click="getAnswer">Go</button>
+    <p>
+        {{ answer }}
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    name: "myFirstComponent",
+    props: {},
+    data() {
+        return {
+            title: "Decision Helper",
+            options: ["yes", "no", "maybe"],
+            answer: "Get your answer here",
+        };
+    },
+    methods: {
+        getAnswer: function () {
+            var randomOptionIndex = Math.floor(Math.random() * this.options.length);
+            this.answer = this.options[randomOptionIndex];
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
